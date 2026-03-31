d6b64322fdb3e4efbc8d02f8ab4b659ba026a497
Gloria R <norimaqidoe@hotmail.com>
2026-03-31 01:44:34 -0600

    fix(api): switch to Bearer token for TMDB authentication

--- src/utils/tmdbApi.js
+++ src/utils/tmdbApi.js
@@ -1,7 +1,15 @@
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY_TMDB = import.meta.env.VITE_TMDB_API_KEY_TMDB;
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${TOKEN}`,
+  },
+};
 
 function handleResponse(res) {
   if (res.ok) {
@@ -11,31 +19,32 @@ function handleResponse(res) {
 }
 
 export function getTrendingMovies() {
-  return fetch(
-    `${BASE_URL}/movie/popular?API_KEY_TMDB=${API_KEY_TMDB}&language=es-MX&page=1`,
-  ).then(handleResponse);
+  return fetch(`${BASE_URL}/movie/popular?language=es-MX&page=1`, options).then(
+    handleResponse,
+  );
 }
 
 export function getTrendingSeries() {
-  return fetch(
-    `${BASE_URL}/tv/popular?API_KEY_TMDB=${API_KEY_TMDB}&language=es-MX&page=1`,
-  ).then(handleResponse);
+  return fetch(`${BASE_URL}/tv/popular?language=es-MX&page=1`, options).then(
+    handleResponse,
+  );
 }
 
 export function searchMoviesAndSeries(query) {
   return fetch(
-    `${BASE_URL}/search/multi?API_KEY_TMDB=${API_KEY_TMDB}&query=${encodeURIComponent(query)}&language=es-MX&page=1`,
+    `${BASE_URL}/search/multi?query=${encodeURIComponent(query)}&language=es-MX&page=1`,
+    options,
   ).then(handleResponse);
 }
 
 export function getMovieById(id) {
-  return fetch(
-    `${BASE_URL}/movie/${id}?API_KEY_TMDB=${API_KEY_TMDB}&language=es-MX`,
-  ).then(handleResponse);
+  return fetch(`${BASE_URL}/movie/${id}?language=es-MX`, options).then(
+    handleResponse,
+  );
 }
 
 export function getSeriesById(id) {
-  return fetch(
-    `${BASE_URL}/tv/${id}?API_KEY_TMDB=${API_KEY_TMDB}&language=es-MX`,
-  ).then(handleResponse);
+  return fetch(`${BASE_URL}/tv/${id}?language=es-MX`, options).then(
+    handleResponse,
+  );
 }
