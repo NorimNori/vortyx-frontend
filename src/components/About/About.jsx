042d1a312880fa2969f7fddb791e135741bf7cd9
Gloria R <norimaqidoe@hotmail.com>
2026-04-10 00:50:05 -0600

    feat(ui): add css styles for About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,14 +2,19 @@ import ApisSection from "./ApiSection";
 import AuthorSection from "./AuthorSection";
 import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./HeroSection";
+import "./About.css";
 
 function About() {
   return (
-    <div>
-      <HeroSection />
-      <FeaturesSection />
-      <AuthorSection />
-      <ApisSection />
+    <div className="about">
+      <div className="about__orb" aria-hidden="true" />
+
+      <div className="about__container">
+        <HeroSection />
+        <FeaturesSection />
+        <AuthorSection />
+        <ApisSection />
+      </div>
     </div>
   );
 }
