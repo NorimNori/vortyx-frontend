5950296e5ddb14661863e8d24b515816fb2aa4f2
Gloria R <norimaqidoe@hotmail.com>
2026-04-11 00:24:33 -0600

    feat: create ItemPoster component

---
+++ src/components/ItemDetail/ItemPoster.jsx
@@ -0,0 +1,19 @@
+function ItemPoster({ image, title }) {
+  return (
+    <aside className="item-detail__poster-wrap">
+      {image ? (
+        <img
+          className="item-detail__poster"
+          src={image}
+          alt={`Portada de ${title}`}
+        />
+      ) : (
+        <div className="item-detail__poster-placeholder" aria-hidden="true">
+          {title?.slice(0, 4).toUpperCase()}
+        </div>
+      )}
+    </aside>
+  );
+}
+
+export default ItemPoster;
