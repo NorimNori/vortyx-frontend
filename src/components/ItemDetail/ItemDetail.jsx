09d795cb451c04721c33cd42030d7a0008acec4d
Gloria R <norimaqidoe@hotmail.com>
2026-04-11 23:58:45 -0600

    feat(ui): add css styles for ItemDetail component

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,6 +4,7 @@ import Preloader from "../Preloader/Preloader";
 import ItemBreadcrumb from "./ItemBreadcrumb";
 import ItemPoster from "./ItemPoster";
 import ItemInfo from "./ItemInfo";
+import "./ItemDetail.css";
 
 const ERROR_MESSAGE =
   "Lo sentimos, algo ha salido mal durante la solicitud. Es posible que haya un problema de conexión o que el servidor no funcione. Por favor, inténtalo más tarde.";
