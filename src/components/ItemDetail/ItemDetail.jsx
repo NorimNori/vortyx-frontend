35df55f38fc61b6fdb6c6ff0ba5e24dc61abf8cc
Gloria R <norimaqidoe@hotmail.com>
2026-04-11 22:45:27 -0600

    feat: create ItemSaveSection component

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,13 @@
 import ItemBreadcrumb from "./ItemBreadcrumb";
 import ItemPoster from "./ItemPoster";
+import ItemSaveSection from "./ItemSaveSection";
 
 export default function ItemDetail() {
   return (
     <div>
       <ItemBreadcrumb />
       <ItemPoster />
+      <ItemSaveSection />
     </div>
   );
 }
