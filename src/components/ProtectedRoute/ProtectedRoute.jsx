c00ae6c616e3cc29af3e3c46a353789730ff74d1
Gloria R <norimaqidoe@hotmail.com>
2026-04-24 00:39:02 -0600

    feat: add ProtectedRoute component for route authorization

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,3 +1,10 @@
-export default function ProtectedRoute() {
-  return <div>ProtectedRoute</div>;
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ isLoggedIn, children }) {
+  if (!isLoggedIn) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
 }
+
+export default ProtectedRoute;
